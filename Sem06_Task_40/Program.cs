7e0b240e36853f3337f9b99e95073cd136b4a8ab
Yuriy <databatonas@gmail.com>
2023-03-13 21:56:06 +0300

    Каждая сторона треугольника меньше суммы двух других сторон

---
+++ Sem06_Task_40/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 40. 
+/* Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать 
+треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.*/
+
+Console.WriteLine("Введите три числа.");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a < b+c && b < a+c && c < a+b) Console.WriteLine("Треугольник существует.");
+else Console.WriteLine("Треугольник не существует.");
